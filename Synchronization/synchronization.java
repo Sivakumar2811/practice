19477456f976c6e5709f473406e2137c8580a7e5
Sivakumar2811 <93197212+Sivakumar2811@users.noreply.github.com>
2021-11-19 11:37:30 +0530

    Add files via upload

---
+++ Synchronization/synchronization.java
@@ -0,0 +1,28 @@
+package Synchronization;
+
+class tables{
+    synchronized void printTable(int n){
+        for(int i=1;i<=5;i++){
+            System.out.println(n*i);
+            try{
+                Thread.sleep(400);
+            }catch(Exception e){System.out.println(e);}
+        }
+    }
+}
+
+public class synchronization {
+    public static void main(String args[]){
+        tables obj = new tables();
+
+        Thread t1 = new Thread(){
+            public void run(){obj.printTable(5);}
+        };
+        Thread t2 = new Thread(){
+            public void run(){obj.printTable(100);}
+        };
+
+        t1.start();
+        t2.start();
+    }
+}
